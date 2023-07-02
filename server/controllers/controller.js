4e661895f2466e0372c0196557126c450d413aca
avoidTheLite <metalsupremacist@gmail.com>
2023-07-02 09:16:40 -0400

    added viewtranscript route

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -19,3 +19,17 @@ export async function sendUserMessage(req, res) {
 
 return chatGPTMessage
 };
+
+
+export function viewtranscript(req,res) {
+    const transcript = messages;
+    switch (transcript.length<2){
+        case true:
+            transcript[0].content = "You have not sent or received any messages with the agent yet. Send a message to begin a conversation."
+            return transcript
+        case false:
+            return transcript
+
+    }
+
+};
